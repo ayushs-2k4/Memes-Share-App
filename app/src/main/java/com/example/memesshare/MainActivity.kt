1a72e3a8abd613d0223e4b1e5e3c5ed22801975e
Ayush Singhal <ayushsinghals02@gmail.com>
2023-05-29 21:52:15 +0530

    Added Predictive Back Gesture

--- app/src/main/java/com/example/memesshare/MainActivity.kt
+++ app/src/main/java/com/example/memesshare/MainActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import androidx.core.view.isVisible
 import com.android.volley.Request
